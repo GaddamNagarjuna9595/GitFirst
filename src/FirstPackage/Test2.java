7a289afe97735d3e7fd23dfac1d416674d5680da
HP <HP@DESKTOP-UTC920B>
2023-04-14 20:42:01 +0530

    First Commit..............

---
+++ src/FirstPackage/Test2.java
@@ -0,0 +1,17 @@
+package FirstPackage;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		String Name= "RamCharan";
+
+		if (Name=="RamCharan") {
+			System.out.println("He IS the Worlds Gretest Lover");
+		}else {
+			System.out.println("Oscar Award WInner");
+		}
+	}  
+}
+
+
